e8d5266a00a43382b2117f658b0db92436efbd28
julivito22 <julietagimenez_6@hotmail.com>
2023-05-11 18:17:13 -0300

    PI DOGS

--- api/src/models/Temperament.js
+++ api/src/models/Temperament.js
@@ -4,10 +4,19 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('temperamento', {
+    id: {
+      type:DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
     name: {
         type: DataTypes.STRING,
         allowNull: false,
       },
-   });
+   },
+   {
+    timestamps: false,
+   }
+   );
 
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,6 +3,7 @@ const axios = require ('axios');
 const {Dog, Temperamento} = require ("../db");
 const { APIKEY } = process.env;
 
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -12,15 +13,16 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 const getApiInfo = async () => {
-    const apiUrl = await axios.get(`https://api.thedogapi.com/v1/breeds/?api_key=${APIKEY}`);
+    const apiUrl = await axios.get('https://api.thedogapi.com/v1/breeds');
     const apiInfo = await apiUrl.data.map(el => {
         return {
             name: el.name,
-            img: el.img,
-            id: el.char_id,
+            image: el.image,
+            id: el.id,
             height: el.height,
             weight: el.weight,
-            yearsOfLife: el.yearsOfLife
+            life_span: el.life_span,
+            temperament: el.temperament
         };
     });
     return apiInfo;
@@ -30,7 +32,7 @@ const getDbInfo = async () => {
     return await Dog.findAll({
         include: {
             model: Temperamento,
-            attributes: ['name'],
+            attributes: ['id', 'name'],
             through: {
                 attributes: [],
             },
@@ -49,38 +51,88 @@ const getAllDogs = async () =>{
 router.get('/dogs', async (req, res) =>{
     const breed = req.query.name
     let dogsTotal = await getAllDogs();
+    
     if(breed){
-        let dogsBreeds = await dogsTotal.filter(el => el.breed.tolowerCase().includes(breed))
+        let dogsBreeds = await dogsTotal.filter(el => el.breed.tolowerCase().includes(id, name, height, weight, breed, yearsOfLife, image))
         dogsBreeds.lenght ?
         res.status(200).send(dogsBreeds) :
         res.status(404).send('No existe la raza');
     } else{
         res.status(200).send(dogsTotal)
     }
-})
+});
 
-router.get('temperaments', async(req,res) => {
-    const temperamentsApi = await axios.get(`https://api.thedogapi.com/v1/breeds/?api_key=${APIKEY}`)
-    const temperaments = temperamentsApi.data.map(el => el.temperament)
-    const occEach = temperaments.map(el => {
-        for (let i=0; i<el.lenght; i++) return el[i]})
-        console.log(occEach)
-        occEach.forEach(el => {
-            Temperamento.findOrCreate({
-                where: {name: el}
-            })
-        })
-        const allTemperaments = await Temperamento.findAll();
-        res.send(allTemperaments);
-})
+
+router.get('/dogs/name', async (req, res) => {
+    const {name} = req.query;
+    try {
+        const dogDB = await Dog.findAll({
+            where: {
+                name: {
+                    [Op.iLike]: `%${name}%`
+                }
+            },
+            include: {
+                model: Temperamento,
+                attributes: ['name'],
+                through: {
+                    attributes: []
+                }
+            }
+        });
+
+        const response = await axios.get(`https://api.thedogapi.com/v1/breeds/${name}`);
+        const dogApi = response.data.map(dog => ({
+            id: dog.id,
+            name: dog.name,
+            image: dog.image.url,
+            temperament: dog.temperament?.split(','.map(e => e.trim())) || []
+        }));
+
+        const dogs = [...dogDB, ...dogApi];
+
+        if (dogs.lenght === 0) {
+            return res.status(404).json({error: 'No hay ningun perro con ese nombre'});
+        }
+        res.json(dogs);
+    }catch (error) {
+        console.log(error);
+        res.status(500).json({error: error.message});
+    }
+});
+
+
+
+router.get('/temperaments', async(req,res) => {
+    try {
+        let temperament = await Temperamento.findAll();
+        if(temperament.lenght === 0) {
+            const response = await axios.get('https://api.thedogapi.com/v1/breeds');
+            console.log(response.data.results);
+            const apiTemperament = response.data.results;
+            apiTemperament.forEach(async (temperament) => {
+                const newTemperament = new Temperamento({name: temperament.name});
+                await newTemperament.save();
+                console.log( `Se guardo correctamente ${temperament.name} en la base de datos`);
+            });
+            temperament =  await Temperamento.findAll();
+        }
+        res.json(temperament);
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({error: error.message});
+    }
+});
 
 router.post('/dogs', async (req,res) =>{
-    let {
+    const {
         name,
         height,
         weight,
         image,
-        yearsOfLife
+        life_span, 
+        createdInDb,
+        temperament
 
     } = req.body;
 
@@ -89,7 +141,8 @@ router.post('/dogs', async (req,res) =>{
         height,
         weight,
         image,
-        yearsOfLife,
+        life_span,
+        createdInDb
 
     })
 
@@ -99,16 +152,28 @@ router.post('/dogs', async (req,res) =>{
     res.send('Dog created succesfully!')
 });
 
-router.get('/dogs/:idRaza', async (req, res) =>{
-    const id = req.params.id;
-    const dogsTotal = await getAllDogs()
-    if(id){
-        let dogId = await dogsTotal.filter(el => el.id == id)
-        dogId.lenght?
-        res.status(200).json(dogId) :
-        res.status(404).send('I did not find that dog')
+router.get('/dogs/:id', async (req,res) => {
+    const {id} = req.params;
+    try {
+        const response = await axios.get (`https://api.thedogapi.com/v1/breeds/${id}`);
+
+        const dogId ={
+            name: response.data.name,
+            height: response.data.height.metric,
+            weight: response.data.weight.metric,
+            life_span: response.data.life_span,
+            temperament: response.data.temperament,
+            reference_image_id: response.data.reference_image_id,
+        };
+        res.status(200).json(dogId)
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('No se encontro el ID del personaje');
     }
-})
+
+    
+ });
+
 
 
 module.exports = router;
